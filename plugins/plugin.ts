2b52a4d347463283ce8c98f06d89f413181e59b0
Jian <liujian394@163.com>
2021-03-26 00:54:15 +0800

    feat: 增加debugger

--- plugins/plugin.ts
+++ plugins/plugin.ts
@@ -5,32 +5,35 @@ const pluginName = 'Plugin1'
 
 class Plugin1 {
     apply(compiler: Compiler) {
-        compiler.hooks.environment.tap(pluginName, () => {
-            console.log('初始化插件之后立即调用');
+        // compiler.hooks.environment.tap(pluginName, () => {
+        //     console.log('初始化插件之后立即调用');
+        // })
+        // compiler.hooks.afterCompile.tap(pluginName, () => {
+        //     console.log('初始化插件完成后立即调用')
+        // })
+        // compiler.hooks.run.tap(pluginName, (compiler) => {
+        //     console.log('开始打包');
+        //     console.log(compiler.options.context);
+        // })
+        //
+        // compiler.hooks.emit.tapAsync(pluginName, (compilation, cb) => {
+        //     setTimeout(() => {
+        //         console.log('emit.tap 1111')
+        //         cb()
+        //     }, 1000)
+        // })
+        //
+        // compiler.hooks.afterEmit.tap(pluginName, (compilation) => {
+        //     console.log('afterEmit.tap 1111')
+        // })
+        //
+        // compiler.hooks.done.tap(pluginName, (stats) => {
+        //     console.log('done.tap 1111')
+        // })
+        compiler.hooks.thisCompilation.tap(pluginName,compilation => {
+            debugger
+            console.log(compilation)
         })
-        compiler.hooks.afterCompile.tap(pluginName, () => {
-            console.log('初始化插件完成后立即调用')
-        })
-        compiler.hooks.run.tap(pluginName, (compiler) => {
-            console.log('开始打包');
-            console.log(compiler.options.context);
-        })
-
-        compiler.hooks.emit.tapAsync(pluginName, (compilation, cb) => {
-            setTimeout(() => {
-                console.log('emit.tap 1111')
-                cb()
-            }, 1000)
-        })
-
-        compiler.hooks.afterEmit.tap(pluginName, (compilation) => {
-            console.log('afterEmit.tap 1111')
-        })
-
-        compiler.hooks.done.tap(pluginName, (stats) => {
-            console.log('done.tap 1111')
-        })
-
     }
 }
 
