53a0f1e4db034d91a8ef5fa833b6145b3046bbaf
Jian <liujian394@163.com>
2021-04-01 22:46:30 +0800

    feat: 创建模板分支

--- plugins/plugin.ts
+++ plugins/plugin.ts
@@ -1,55 +1,11 @@
 import {
-    Compiler,
-    sources
+    Compiler
 } from 'webpack'
-import fs from 'fs/promises'
 
 const pluginName = 'Plugin1'
-const {RawSource} = sources
 class Plugin1 {
     apply(compiler: Compiler) {
-        // compiler.hooks.environment.tap(pluginName, () => {
-        //     console.log('初始化插件之后立即调用');
-        // })
-        // compiler.hooks.afterCompile.tap(pluginName, () => {
-        //     console.log('初始化插件完成后立即调用')
-        // })
-        // compiler.hooks.run.tap(pluginName, (compiler) => {
-        //     console.log('开始打包');
-        //     console.log(compiler.options.context);
-        // })
-        //
-        // compiler.hooks.emit.tapAsync(pluginName, (compilation, cb) => {
-        //     setTimeout(() => {
-        //         console.log('emit.tap 1111')
-        //         cb()
-        //     }, 1000)
-        // })
-        //
-        // compiler.hooks.afterEmit.tap(pluginName, (compilation) => {
-        //     console.log('afterEmit.tap 1111')
-        // })
-        //
-        // compiler.hooks.done.tap(pluginName, (stats) => {
-        //     console.log('done.tap 1111')
-        // })
-        compiler.hooks.thisCompilation.tap(pluginName,(compilation) => {
-            // debugger
-            compilation.hooks.additionalAssets.tapPromise(pluginName,async () => {
-                const content = 'Hello plugin1'
-                // @ts-ignore
-                compilation.assets['a.txt'] = {
-                    size(): number {
-                        return content.length;
-                    },
-                    source(): string | Buffer {
-                        return content;
-                    }
-                }
-                return Promise.resolve();
-            })
 
-        })
     }
 }
 
